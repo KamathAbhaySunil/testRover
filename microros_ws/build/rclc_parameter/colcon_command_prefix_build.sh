72f3a54b96758ffb36a785e95fe5aa7b6bca4d4d
KamathAbhaySunil <abhaysk23@gmail.com>
2024-06-27 01:14:10 +0530

    first commit

--- microros_ws/build/rclc_parameter/colcon_command_prefix_build.sh
+++
@@ -1,8 +0,0 @@
-# generated from colcon_core/shell/template/command_prefix.sh.em
-. "/home/abhay/microros_ws/install/builtin_interfaces/share/builtin_interfaces/package.sh"
-. "/home/abhay/microros_ws/install/unique_identifier_msgs/share/unique_identifier_msgs/package.sh"
-. "/home/abhay/microros_ws/install/action_msgs/share/action_msgs/package.sh"
-. "/home/abhay/microros_ws/install/rcl_interfaces/share/rcl_interfaces/package.sh"
-. "/home/abhay/microros_ws/install/std_msgs/share/std_msgs/package.sh"
-. "/home/abhay/microros_ws/install/example_interfaces/share/example_interfaces/package.sh"
-. "/home/abhay/microros_ws/install/rclc/share/rclc/package.sh"
--- microros_ws/build/sensor_msgs_py/prefix_override/sitecustomize.py
+++
@@ -1,3 +0,0 @@
-import sys
-sys.real_prefix = sys.prefix
-sys.prefix = sys.exec_prefix = '/home/abhay/microros_ws/install/sensor_msgs_py'
